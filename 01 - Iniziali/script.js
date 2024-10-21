21a686cb72599253b8d9a2679dc02ae53040d876
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-21 15:31:08 +0200

    add first exercise

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,12 +1,24 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const firstLetter = [];
 
 // Dichiara la funzione qui.
+const firstLetterSelect = ( firstLetter ,names ) =>{
+    console.log(firstLetter, names);
+    for(let i= 0; i < names.length ; i++){
+        const currentElement = names[i];      
+        firstLetter.push(currentElement.charAt(0));
+
+    }
+    return firstLetter;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result= firstLetterSelect(firstLetter,names)
+console.log(result);
 
 
 
--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,9 +1,27 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const searchedLetter = "A"
 
 // Dichiara la funzione qui.
+const firstLetterSelect = ( searchedLetter ,names ) =>{
+    console.log(searchedLetter, names);
+    const searchedElement =[];
+    for(let i= 0; i < names.length ; i++){
+        const currentElement = names.length[i]; 
+        if(currentElement.charAt(0) === searchedLetter){
+        searchedElement.push(currentElement);
+        }
+        
+    return searchedElement;
+
+
+    }
+
+    
+    
+}
+const result= firstLetterSelect(searchedLetter,names)
 
 
 // Invoca la funzione qui e stampa il risultato in console
